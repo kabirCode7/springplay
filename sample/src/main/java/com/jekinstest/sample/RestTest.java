ac2a7323efb5bb54affd7530b462771ced755f89
Imam Kabir <bob@exa>
2019-05-02 16:00:07 +0530

    change2

--- sample/src/main/java/com/jekinstest/sample/RestTest.java
+++ sample/src/main/java/com/jekinstest/sample/RestTest.java
@@ -1,5 +1,15 @@
 package com.jekinstest.sample;
 
-public class RestTest {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(path = "/employees")
+public class RestTest {
+	 @GetMapping(path="/", produces = "application/json")
+	    public String getEmployees()
+	    {
+	        return "hello world";
+	    }
 }
